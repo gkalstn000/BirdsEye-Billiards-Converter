f5b7a54c4583f9b4b129e97428713bfbda3c6d00
Choyongil <cyi1207@naver.com>
2020-04-06 16:26:49 +0900

    c

--- trans/imgwarp2.py
+++ trans/imgwarp2.py
@@ -1,90 +1,47 @@
 import numpy as np
 import cv2
-from PIL import Image
 
-def load_image_into_numpy_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
-'''
-image_path = '/Users/gkalstn/capstone/test_images/img137.jpg'
-image = Image.open(image_path)
-image = image.resize((845,526))
-image_np = load_image_into_numpy_array(image)
-'''
-def img_warp(total_7_list, image_np):
 
-    mylist = []
-    for i in total_7_list:
-        mylist.append(tuple(i))
+def warp(array):
 
-    point_list = mylist[0:4]   # 당구대 좌표 배열
-    point_list2 = mylist[4:7]    # 공 좌표 배열
+    img_original2 = cv2.imread('base.png')  # test3.jpg 파일을 img_original 변수에 저장
+    img_result2 = cv2.resize(img_original2, dsize=(348,630), interpolation=cv2.INTER_AREA)
 
-    ##### 한글인식 못함, 에러발생 '빈다이' -> base, '위치2-1' -> test #####
-    img_original = image_np # test.jpg 파일을 img_original 변수에 저장
-    
-    img_original2 = cv2.imread('./trans/base.png') # test3.jpg 파일을 img_original 변수에 저장
-    img_original2 = cv2.resize(img_original2, dsize=(650, 344), interpolation=cv2.INTER_AREA)
-    
-
-    #(B,G,R)
-    cv2.circle(img_original, (point_list2[0]), 2, (255, 255, 255), -1) # R -> W
-    cv2.circle(img_original, (point_list2[1]), 2, (0, 0, 255), -1) # B -> R
-    cv2.circle(img_original, (point_list2[2]), 2, (0, 255, 255), -1) # G -> Y
-
-    height, width = 315, 612 # return 되는 이미지의 크기 값
+    width, height = 315, 612  # return 되는 이미지의 크기 값
 
     # 좌표 순서 - 상단왼쪽 끝, 상단오른쪽 끝, 하단왼쪽 끝, 하단오른쪽 끝
-    pts1 = np.float32([list(point_list[0]),list(point_list[1]),list(point_list[2]),list(point_list[3])])
-    pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+    pts1 = np.float32([list(array[0]), list(array[1]), list(array[2]), list(array[3])])
+    pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
 
-    print('pts1 : ',pts1)
-    print('pts2 : ',pts2)
+    M = cv2.getPerspectiveTransform(pts1, pts2)  # pts1의 좌표를 pts2의 좌표로 변환 시킬 변수 M 설정
 
-    M = cv2.getPerspectiveTransform(pts1,pts2)      # pts1의 좌표를 pts2의 좌표로 변환 시킬 변수 M 설정
-    a = 1
-    b = 2
+    ball_list = []
 
-    img_result = cv2.warpPerspective(img_original, M, (width,height))      # 이미지 와핑
-    img_result2 = img_original2
+    brr = []
+    for i in range(4,7):
+        temp = [array[i][0], array[i][1], 1]
+        temp = np.reshape(temp, (3, 1))
 
-    white=0     # white볼을 둘 공간을 찾았을 때 조건문을 돌리지 않기 위한 변수 R -> W
-    red=0     # red볼을 둘 공간을 찾았을 때 조건문을 돌리지 않기 위한 변수 B -> R
-    yellow=0     # yellow볼을 둘 공간을 찾았을 때 조건문을 돌리지 않기 위한 변수 G -> Y
+        brr.append(np.dot(M, temp))
+        x = brr[i-4][0] / brr[i-4][2] + 18
+        y = brr[i - 4][1] / brr[i - 4][2] + 18
+        ball_list.append((x, y))
 
-    for y in range(15,300):
-        for x in range(15,610):
-            if img_result[y,x][0] == 255 and img_result[y,x][1] == 255 and img_result[y,x][2] == 255 and white == 0:
-                img_result2 = cv2.circle(img_result2, (x+25,y+14),7,(255,255,255),-1)       # 해당 좌표값에 공 그리기
-                white = white + 1
-                print('<white>\ny, x :',y,', ',x)
-            elif img_result[y,x][0] == 0 and img_result[y,x][1] == 255 and img_result[y,x][2] == 255 and yellow == 0:
-                img_result2 = cv2.circle(img_result2, (x+25,y+14),7,(0,255,255),-1)
-                yellow = yellow + 1
-                print('<yellow>\ny, x :',y,', ',x)
-            elif img_result[y,x][0] == 0 and img_result[y,x][1] == 0 and img_result[y,x][2] == 255 and red == 0:
-                img_result2 = cv2.circle(img_result2, (x+25,y+14),7,(0,0,255),-1)
-                red = red + 1
-                print('<red>\ny, x :',y,', ',x)
-            if white == 1 and red == 1 and yellow == 1:
-                k=1
-                break
-        if white == 1 and red == 1 and yellow == 1:
-            break
+    img_result2 = cv2.circle(img_result2, ball_list[0], 10, (255, 255, 255), -1)  # 해당 좌표값에 공 그리기
+    img_result2 = cv2.circle(img_result2, ball_list[1], 10, (0, 0, 255), -1)
+    img_result2 = cv2.circle(img_result2, ball_list[2], 10, (0, 255, 255), -1)
+
+    # return cv2.imshow("result2", img_result2)
+
+    cv2.imwrite("show_result.png", img_result2)
 
 
-    print('\nok I know where they are')
-    try :
-        cv2.imshow("result1", img_original)
-        cv2.imshow("result2", img_result2)
-        
-        ##### 마지막 결과 이미지 저장 #####
-        cv2.imwrite("show_result.png", img_result2)
-        # cv2.imshow("result3", img_original3)
-        cv2.waitKey(0)
-    except Exception as e:
-        print(str(e))
-    # ##### 이미지파일로 리턴 #####
-    # return(cv2.imread("show_result.png"))
+# 예시 7가지
 
-#img_warp([[256, 500], [736, 328], [225, 302], [571, 0], [225, 137], [385, 141], [539, 141]],image_np)
+# warp([(360, 683), (805, 554), (9, 310), (322, 312), (241, 518), (444, 487), (595, 466)])
+# warp([(23, 464), (817, 444), (226, 94), (525, 98), (226, 142), (384, 142), (539, 142)])
+# warp([(3, 346), (558, 501), (488, 85), (795, 99), (454, 156), (572, 139), (681, 121)])          # 위치 1-4
+# warp([(236, 668), (827, 527), (26, 290), (347, 281), (356, 533), (417, 458), (468, 404)])
+# warp([(17, 533), (474, 692), (523, 365), (821, 393), (241, 475), (533, 606), (690, 481)])     # 위치 3-3
+# warp([(60, 533), (598, 654), (510, 355), (810, 368), (272, 467), (444, 491), (660, 525)])       # 위치 2-4
+cv2.waitKey(0)
---
+++ trans/show_result.py
@@ -0,0 +1,16 @@
+import cv2
+
+
+def show_result(orgin_png) :
+
+    left = cv2.imread(orgin_png)    # left = cv2.imread("위치2-1.jpeg") #입력받은 초기 이미지
+    right = cv2.imread("show_result.png")   # 처리 완료한 결과 이미지
+    left = cv2.resize(left, dsize=(344, 650), interpolation=cv2.INTER_AREA)
+
+    result = cv2.hconcat([left, right])     # 두 이미지 합친 마지막 결과
+    cv2.imshow("last result", result)
+    cv2.imwrite("last_result.jpg", result)
+    cv2.waitKey(0)
+
+
+show_result()
